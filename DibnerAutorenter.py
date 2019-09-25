820d94773969e27d71fa2530f2db7abccab2e01e
Kevin Hu <kh2547@nyu.edu>
2019-09-25 14:05:43 -0400

    created a dictionary for room:available times

--- DibnerAutorenter.py
+++ DibnerAutorenter.py
@@ -1,19 +1,30 @@
 from selenium import webdriver
 import time
-from selenium.webdriver.common.keys import Keys
+#import pandas
+
+#Goal: should ask for 3 inputs, date, start time, and hours to book for
+#list of room list, inside each room list, string with starting time
+
+roomList = {}
 
 driver = webdriver.Chrome()
 driver.get("https://nyu.libcal.com/booking/berndibner2")
 
-
 elem = driver.find_elements_by_class_name("lc_rm_a")
-elem2 = driver.find_elements_by_class_name("ui-state-default")
 time.sleep(1)
 
 #possibly map id to date to figure out what to click
 for option in elem:
-    print(option.get_attribute("title"))
+    roomData = (option.get_attribute("title")).split()
+    key = roomData[2]
+    value = roomData[3]
+    if roomData[2] not in roomList.keys():
+        roomList[key] = [value]
+    else:
+        roomList[key].append(value)
 
     #option.click()
 
 driver.close()
+
+print(roomList)
