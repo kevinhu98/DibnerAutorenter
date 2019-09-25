658a9fc48f2645fa6aed6b1eeffc01047dbec099
Kevin Hu <kh2547@nyu.edu>
2019-09-25 16:54:17 -0400

    reformmated dictionary to match user input

--- DibnerAutorenter.py
+++ DibnerAutorenter.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 import time
+import substring
 #import pandas
 
 #Goal: should ask for 3 inputs, date, start time, and hours to book for
 #list of room list, inside each room list, string with starting time
+
+#day = input("What day do you want to rent? ")
+#startTime = input("What time do you want to rent your room? Please enter number then am/pm. ")
+#hours = input("How many hours would you like to rent your room for? ")
 
 roomList = {}
 
@@ -11,17 +16,25 @@ driver = webdriver.Chrome()
 driver.get("https://nyu.libcal.com/booking/berndibner2")
 
 elem = driver.find_elements_by_class_name("lc_rm_a")
-time.sleep(1)
+time.sleep(1) #delay to load page
 
-#possibly map id to date to figure out what to click
 for option in elem:
-    roomData = (option.get_attribute("title")).split()
-    key = roomData[2]
-    value = roomData[3]
-    if roomData[2] not in roomList.keys():
+    singleRoomData = option.get_attribute("title") #grab each room data
+    singleRoomData = singleRoomData.replace(',','') #grab
+    singleRoomData = singleRoomData.split()
+    key = singleRoomData[2] #roomID
+    value = singleRoomData[3] #room initial time
+    print(singleRoomData)
+    if len(value) == 7:
+        value = value[0:2] + value[5:7]
+    else:
+        value = value[0] + value[4:6]
+
+    if singleRoomData[2] not in roomList.keys():
         roomList[key] = [value]
     else:
         roomList[key].append(value)
+
 
     #option.click()
 
